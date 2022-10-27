ce994dad3df866665938078fd3604bfccfa20b48
Chuykov, Petr <petr.chuykov@x5.ru>
2022-10-27 10:26:45 +0300

    Prepare repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="alber",
+    version="1.0",
+    author="Peter Chuikov",
+    packages=["alber"],
+    zip_safe=False,
+    # install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+)
